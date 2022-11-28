d444d0bbdde740b70a61cb5997216365c4203803
Linh Nguyen <nvvlinh2001@gmail.com>
2022-11-28 17:26:11 +0700

    HT

--- frontend/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'vuex';
-import nhanvien from './modules/nhanvien';
-import khachhang from './modules/khachhang';
-export default createStore({
-  modules: {
-    nhanvien,
-    khachhang,
-  }
-})
--- frontend/src/views/NotFound.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="page pt-5">
-      <p class="text-center">
-        Oops, không thể tìm thấy trang. Trở về
-        <router-link to="/admin">trang chủ.</router-link>
-      </p>
-    </div>
-  </template>
